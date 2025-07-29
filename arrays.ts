92a6fe21a46d0934bc400296d2fc6b32b84c2bb4
Mohd Faziel <mohdfazel969@gmail.com>
2025-07-29 12:29:33 +0530

    Learned tuples

--- arrays.ts
+++ arrays.ts
@@ -4,3 +4,6 @@ let arr2 :readonly (number|string)[] = [1,2,3,"faz"];
 //@ts-ignore
 arr2.push(2);
 let arr3 = [1,2,"asf",[],{},null,undefined];
+
+//tuples: Fixed length array
+let coordinates: readonly [number,number] = [123,2423];
