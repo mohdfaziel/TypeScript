21fa865a37621bb8c445a084a40b8a5d7669c029
Mohd Faziel <mohdfazel969@gmail.com>
2025-07-29 12:47:23 +0530

    Learned objects

---
+++ objects.ts
@@ -0,0 +1,15 @@
+let student : {
+    name : string,
+    rollNo : number,
+    age : number
+} = {
+    name : "Faziel",
+    rollNo : 107,
+    age : 22
+}
+
+
+let students : {
+    name : string,
+    rollNo : number,
+    age : number }[] = [student, student];
