71bd7bdcb2c946ec4ec312df5d8280de234a2e2a
Mohd Faziel <mohdfazel969@gmail.com>
2025-07-29 12:14:30 +0530

    Learned Arrays

---
+++ arrays.ts
@@ -0,0 +1,6 @@
+let arr = [1,3,4,5,"fd", "hello", 2.5, true];
+console.log(arr);
+let arr2 :readonly (number|string)[] = [1,2,3,"faz"];
+//@ts-ignore
+arr2.push(2);
+let arr3 = [1,2,"asf",[],{},null,undefined];
