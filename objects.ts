1871453d8e56a7129eb85c14be790dbe77913e5a
Mohd Faziel <mohdfazel969@gmail.com>
2025-07-31 11:56:06 +0530

    Type alias

--- objects.ts
+++ objects.ts
@@ -13,3 +13,15 @@ let students : {
     name : string,
     rollNo : number,
     age : number }[] = [student, student];
+
+type User = {
+    name: string;
+    age: number;
+    rank?:number
+}
+
+let user1: User = {
+    name:"Faziel",
+    age:22,
+    rank:482
+}
