0ff25c2bc89d5daa585b970b6dd8fa31a3f776c2
Mohd Faziel <mohdfazel969@gmail.com>
2025-08-21 01:03:38 +0530

    Generic in class

--- dist/generic.js
+++ dist/generic.js
@@ -10,3 +10,27 @@ function getObjVal(obj, key) {
     return obj[key];
 }
 console.log(getObjVal({ "name": "faziel" }, "name"));
+//generic in class
+class ItemStorage {
+    constructor() {
+        this.items = [];
+    }
+    getItems() {
+        console.log(this.items);
+    }
+    addItem(item) {
+        this.items.push(item);
+    }
+    removeItem(item) {
+        this.items = this.items.filter((i) => i != item);
+    }
+}
+const strStorage = new ItemStorage();
+strStorage.addItem("faz");
+const numStorage = new ItemStorage();
+numStorage.addItem(123);
+strStorage.getItems();
+numStorage.getItems();
+const user10 = new ItemStorage();
+user10.addItem({ name: "Faziel", age: 12 });
+user10.getItems();
--- src/generic.ts
+++ src/generic.ts
@@ -14,3 +14,37 @@ function getObjVal<T extends object, U extends keyof T>(obj : T,key : U)
 }
 
 console.log(getObjVal({"name":"faziel"},"name"));
+
+//generic in class
+class ItemStorage<T>
+{
+    private items : T[] = [];
+    getItems()
+    {
+        console.log(this.items);
+    }
+    addItem(item : T)
+    {
+        this.items.push(item);
+    }
+    removeItem(item : T)
+    {
+        this.items = this.items.filter((i) => i!=item);
+    }
+}
+
+const strStorage = new ItemStorage<string>();
+strStorage.addItem("faz");
+const numStorage = new ItemStorage<number>();
+numStorage.addItem(123);
+strStorage.getItems();
+numStorage.getItems();
+
+interface Users{
+    "name":string;
+    "age":number;
+}
+
+const user10 = new ItemStorage<Users>();
+user10.addItem({name:"Faziel",age:12})
+user10.getItems();
